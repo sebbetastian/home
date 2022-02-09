87519f5ab9740e8f8016f3e6907fe54d18c5941d
Sebastian Aanstad <sebbeaanstad@gmail.com>
2022-02-09 22:04:20 +0100

    not much

--- scripts/login.js
+++ scripts/login.js
@@ -8,15 +8,17 @@ var dT = (function disTime() {
     displayTime.textContent = 'Time & Date: ' + dateTime;
 }())
 
-const message = document.getElementById('message');
-const param = new URLSearchParams(window.location.search);
-const username = param.get('username');
-const pw = param.get('password');
+var loIn = (function logIn() {
+    const message = document.getElementById('message');
+    const param = new URLSearchParams(window.location.search);
+    const username = param.get('username');
+    const pw = param.get('password');
 
-if(username.toLowerCase() === 'name' && pw === 'password') {
-    message.innerHTML = 'Welcome User!';
-}   else if(!username || !pw){
-    message.innerHTML = 'who are you?';
-}   else {
-    message.innerHTML = 'find the password!';
-}
+    if(username.toLowerCase() === 'name' && pw === 'password') {
+        message.innerHTML = 'Welcome User!';
+    }   else if(!username || !pw){
+        message.innerHTML = 'who are you?';
+    }   else {
+        message.innerHTML = 'find the password!';
+    }
+}())
