7701c9df3919c625aa39af715f26a70d5e9c0b96
Sebastian Aanstad <sebbeaanstad@gmail.com>
2022-03-16 01:05:41 +0100

    bug squash

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,3 @@
-var dT = (function disTime() {
-    var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes();
-    var dateTime = date+' '+time;
-    
-    const displayTime = document.getElementById('timeDate');
-    displayTime.textContent = 'Time & Date: ' + dateTime;
-}())
-
-var loIn = (function logIn() {
-    const message = document.getElementById('message');
-    const param = new URLSearchParams(window.location.search);
-    const username = param.get('username');
-    const pw = param.get('password');
-
-    if(username.toLowerCase() === 'name' && pw === 'password') {
-        message.innerHTML = 'Welcome User!';
-    }   else if(!username || !pw){
-        message.innerHTML = 'who are you?';
-    }   else {
-        message.innerHTML = 'find the password!';
-    }
-}())
-
 const source = document.getElementById('eachHelper').innerHTML;
 const template = Handlebars.compile(source);
 
@@ -45,7 +20,33 @@ const context = {
   ]
 };
 
+var dT = (function disTime() {
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes();
+    var dateTime = date+' '+time;
+    
+    const displayTime = document.getElementById('timeDate');
+    displayTime.textContent = 'Time & Date: ' + dateTime;
+}())
+
 const compiledHtml = template(context);
 
 const displayElements = document.getElementById('display');
 displayElements.innerHTML = compiledHtml;
+
+var loIn = (function logIn() {
+    const message = document.getElementById('message');
+    const param = new URLSearchParams(window.location.search);
+    const username = param.get('username');
+    const pw = param.get('password');
+
+    if(username.toLowerCase() === 'name' && pw === 'password') {
+        message.innerHTML = 'Welcome User!';
+    }   else if(!username || !pw){
+        message.innerHTML = 'who are you?';
+    }   else {
+        message.innerHTML = 'find the password!';
+    }
+}())
+
